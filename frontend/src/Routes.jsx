6735027f14c4be39404e1ec3d1e6f0fe47876e72
ayushhub-hub <ayushsable87@gmail.com>
2025-03-06 10:35:00 +0530

    Milestone 19 completed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -5,5 +5,6 @@ import CreateProduct from './pages/createProduct';
 import MyProducts from './pages/myProduct';
 import Cart from './pages/cart';
 import ProductDetails from './pages/productDetails';
-import Profile from './pages/profile'
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile};
+import Profile from './pages/profile';
+import CreateAddress from './pages/createAddress.jsx';
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress};
--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AddressCard from "../components/auth/AddressCard";
 import Nav from "../components/auth/nav";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
 	const [personalDetails, setPersonalDetails] = useState({
@@ -9,6 +10,7 @@ export default function Profile() {
 		phoneNumber: "",
 		avatarUrl: "",
 	});
+	const navigate=useNavigate();
 
 	const [addresses, setAddresses] = useState([]);
 
@@ -35,6 +37,10 @@ export default function Profile() {
 				console.log("Addresses fetched:", data.addresses);
 			});
 	}, []);
+
+	const handleAddAddress=()=>{
+		navigate('/create-address')
+	}
 
 
 	return (
@@ -102,7 +108,8 @@ export default function Profile() {
 							</h1>
 						</div>
 						<div className="w-full h-max p-5">
-							<button className="w-max px-3 py-2 bg-neutral-600 text-neutral-100 rounded-md text-center hover:bg-neutral-100 hover:text-black transition-all duration-100"							>
+							<button className="w-max px-3 py-2 bg-neutral-600 text-neutral-100 rounded-md text-center hover:bg-neutral-100 hover:text-black transition-all duration-100"			
+							onClick={handleAddAddress}>
 								Add Address
 							</button>
 						</div>
