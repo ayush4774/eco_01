2c412ba2786871e3dfb1c2cb4c15e61d42e3c88b
ayushhub-hub <ayushsable87@gmail.com>
2025-03-20 14:52:42 +0530

    till M25 done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress} from './Routes.jsx'
+import {LoginPage,SignupPage,Home, CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress,OrderConfirmation} from './Routes.jsx'
 
 import './App.css';
 
@@ -9,16 +9,17 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path= "/create-product" element={<CreateProduct/>}/>
-      <Route path='/create-product/:id' element={<CreateProduct/>}/>
+      <Route path="/signup" element={<SignupPage/>}/>
+      <Route path="/create-product" element={<CreateProduct/>}/>
+      <Route path="/create-product/:id" element={<CreateProduct/>}/>
       <Route path="/my-products" element={<MyProducts/>}/>
       <Route path="/cart" element={<Cart/>}/>
-      <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/product/:id" element={<ProductDetails/>}/>
+      <Route path="/profile" element={<Profile/>}/>
       <Route path="/create-address" element={<CreateAddress/>}/>
-      <Route path='/select-address' element={<SelectAddress/>}/>
-    
+      <Route path="/select-address" element={<SelectAddress/>}/>
+      <Route path="/order-confirmation" element={<OrderConfirmation/>}/>
+
     </Routes>
     </BrowserRouter>
  
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,11 +1,15 @@
 import LoginPage from './pages/Login'
-import SignupPage from './pages/Signup'
-import Home from './pages/Home'
+import SignupPage from './pages/Signup';
+import Home from './pages/Home';
 import CreateProduct from './pages/createProduct';
 import MyProducts from './pages/myProduct';
-import Cart from './pages/cart';
-import ProductDetails from './pages/productDetails';
+import Cart from './pages/cart'
+import ProductDetails from './pages/productDetails'
 import Profile from './pages/profile';
-import CreateAddress from './pages/createAddress.jsx';
-import SelectAddress from './pages/selectAddress.jsx'
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress};
+import CreateAddress from './pages/createAddress';
+import SelectAddress from './pages/selectAddress';
+import OrderConfirmation from './pages/OrderConfirmation';
+
+
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress,OrderConfirmation};
+
